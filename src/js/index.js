2f04d968a5007293127acd40af939cfbeb1162c7
jessicapaola <jessicapaolabm@gmail.com>
2021-04-10 11:38:06 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+function oeste() {
+    console.log('oeste');
+}
+
+function leste() {
+    console.log('leste');
+}
+
+function norte() {
+    console.log('norte');
+}
+
+function abrirMapa() {
+    console.log('abrir mapa');
+}
