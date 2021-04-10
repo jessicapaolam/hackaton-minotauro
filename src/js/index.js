c9b539c7d8e96f405dca09a204563e3e2d866507
jessicapaola <jessicapaolabm@gmail.com>
2021-04-10 14:25:36 -0300

    add border

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,6 @@
-function oeste() {
-    console.log('oeste');
-}
-
-function leste() {
-    console.log('leste');
-}
-
-function norte() {
-    console.log('norte');
-}
-
-function abrirMapa(labirinto, posicao, minotauro, destino) {
+function abrirMapa(labirinto, tamanho, posicao, minotauro, destino) {
     console.log(labirinto);   
+    console.log(tamanho); 
     console.log(posicao);
     console.log(minotauro[0]);
     console.log(destino);
@@ -24,6 +13,17 @@ function abrirMapa(labirinto, posicao, minotauro, destino) {
     document.querySelector('#room' + minotauro[0].x + minotauro[0].y).classList.add('d-none');
     document.querySelector('#room' + destino.x + destino.y).classList.remove('d-show');
     document.querySelector('#room' + destino.x + destino.y).classList.add('d-none');
+
+    for(let i = 0; i < tamanho; i++) {
+        for(let j = 0; j < tamanho; j++) {
+            if (labirinto[i][j].left) {
+                document.querySelector('#border' + i + j).style.borderLeft = '3px solid red';
+            } else if (labirinto[i][j].right) {
+                document.querySelector('#border' + i + j).style.borderRight = '3px solid red';
+            }
+            console.log('dentro do for', labirinto[i][j]);
+        }
+    }
 
     
 }
